e8dec222a0f6cf59605c35c465626ae4219abf10
Ahngyuho <gyuho0897@gmail.com>
2025-11-30 23:03:47 +0900

    feat(config)  :  커스텀 메시지 컨버터를 최우선으로 등록
    내가 만든 MessageConveter 가 가장 먼저 나오게 하기 위한 용도

--- demo/src/main/java/com/example/demo/config/WebConfig.java
+++ demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,15 +8,15 @@ import java.util.List;
 //@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final CommonMessageConverter commonMessageConverter;
+    private final MyStringMessageConverter myStringMessageConverter;
 
-    public WebConfig(CommonMessageConverter commonMessageConverter) {
-        this.commonMessageConverter = commonMessageConverter;
+    public WebConfig(MyStringMessageConverter myStringMessageConverter) {
+        this.myStringMessageConverter = myStringMessageConverter;
     }
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         // 최우선으로
-        converters.add(0, commonMessageConverter);
+        converters.add(0, myStringMessageConverter);
     }
 }
